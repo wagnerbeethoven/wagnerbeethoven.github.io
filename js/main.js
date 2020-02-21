5308936f3d9431c6b78e568613e732c37a01686f
Wagner Beethoven <wagnerbeethoven@gmail.com>
2020-02-21 17:25:15 -0300

    content, layout

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$("#menu ul li a").each(function () {
+  //console.log($(this).attr('href'));
+  if ((window.location.pathname.indexOf($(this).attr('href'))) > -1) {
+    $(this).parent().addClass('menu-item--active');
+  }
+});
+
+var disqus_shortname = 'wagnerbeethoven';
+(function () {
+  var dsq = document.createElement('script');
+  dsq.type = 'text/javascript';
+  dsq.async = true;
+  dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
