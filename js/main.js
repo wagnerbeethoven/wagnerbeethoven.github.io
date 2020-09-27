71c257c0271d0386734f054a95906ad7480ef6d0
Wagner Beethoven <wagnerbeethoven@gmail.com>
2020-09-27 20:31:36 -0300

    toc

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// ADICIONAR MARCAÇÃO DO MENU
+
 $("#menu ul li a").each(function () {
   //console.log($(this).attr('href'));
   if ((window.location.pathname.indexOf($(this).attr('href'))) > -1) {
@@ -5,6 +7,7 @@ $("#menu ul li a").each(function () {
   }
 });
 
+// COMENTÁRIO
 var disqus_shortname = 'wagnerbeethoven';
 (function () {
   var dsq = document.createElement('script');
@@ -14,4 +17,35 @@ var disqus_shortname = 'wagnerbeethoven';
   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 })();
 
+// SVG INJECT
 SVGInject(document.getElementsByClassName('svg'));
+
+// TOC
+$(document).ready(function () {
+  $('#toc').toc();
+});
+
+// ADICIONA _BLANK E NO OPENER E REFERRER
+
+function targetBlank() {
+  // remove subdomain of current site's url and setup regex
+  var internal = location.host.replace("www.", "");
+  internal = new RegExp(internal, "i");
+
+  var a = document.getElementsByTagName('a'); // then, grab every link on the page
+  for (var i = 0; i < a.length; i++) {
+    var href = a[i].host; // set the host of each link
+    if (!internal.test(href)) { // make sure the href doesn't contain current site's host
+      a[i].setAttribute('target', '_blank'); // if it doesn't, set attributes
+      a[i].setAttribute('rel', 'noopener noreferrer'); // if it doesn't, set attributes
+      // a[i].setAttribute('data-toggle', 'tooltip'); // if it doesn't, set attributes
+      a[i].setAttribute('title', 'Este link irá abrir uma nova aba do navegador'); // if it doesn't, set attributes
+
+    }
+  }
+};
+
+targetBlank();
+
+// ADICIONA O LINK NOS Hs
+anchors.add();
