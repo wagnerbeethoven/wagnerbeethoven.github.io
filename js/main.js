2018f01196e1a29ee29f9ab2b3f0f1c0f458ec45
Wagner Beethoven <wagnerbeethoven@gmail.com>
2021-08-14 18:34:49 -0300

    remoção de disqus, google analitcis

--- js/main.js
+++ js/main.js
@@ -1,18 +1,6 @@
 // ADICIONAR MARCAÇÃO DO MENU
 
 
-// COMENTÁRIO
-var disqus_shortname = 'wagnerbeethoven';
-(function () {
-  var dsq = document.createElement('script');
-  dsq.type = 'text/javascript';
-  dsq.async = true;
-  dsq.src = 'http://' + 'wagnerbeethoven' + '.disqus.com/embed.js';
-  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})();
-
-
-
 // TOC
 $(document).ready(function () {
   $('#toc').toc({
