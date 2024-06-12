5d4d20610a04b22aa2b601fc4835bef4f8e08f66
Wagner Beethoven <wagnerbeethoven@gmail.com>
2024-06-12 18:15:35 -0300

    organizaçãodo 'sobre' e a função de imprimit curriculo

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // theme
 
-;(function () {
+; (function () {
   const htmlElement = document.querySelector("html")
   if (htmlElement.getAttribute("data-bs-theme") === 'auto') {
     function updateTheme() {
       document.querySelector("html").setAttribute("data-bs-theme",
-      window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
+        window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
     }
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme)
     updateTheme()
@@ -19,17 +19,22 @@ $(document).ready(function () {
 
 
 // adiciona _blank e diversos
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const baseUrl = window.location.origin;
   const links = document.querySelectorAll('a');
 
   links.forEach(link => {
-      const href = link.getAttribute('href');
-      if (href && href.startsWith('http') && !href.startsWith(baseUrl)) {
-          link.setAttribute('rel', 'external no-referrer noopener');
-          link.setAttribute('target', '_blank');
-          link.setAttribute('title', 'O link irá abrir em uma nova aba');
-          link.setAttribute('referrerpolicy', 'strict-origin');
-      }
+    const href = link.getAttribute('href');
+    if (href && href.startsWith('http') && !href.startsWith(baseUrl)) {
+      link.setAttribute('rel', 'external no-referrer noopener');
+      link.setAttribute('target', '_blank');
+      link.setAttribute('title', 'O link irá abrir em uma nova aba');
+      link.setAttribute('referrerpolicy', 'strict-origin');
+    }
   });
 });
+
+
+function imprimirPagina() {
+  window.print();
+}
