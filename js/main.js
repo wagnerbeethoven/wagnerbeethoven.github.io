7892090f7b4da30cdd4fbeb11800fab8681c11a2
Wagner Beethoven <wagnerbeethoven@gmail.com>
2024-09-24 16:33:08 -0300

    não consegui dar merge das branchs

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 // theme
 
-; (function () {
-  const htmlElement = document.querySelector("html")
-  if (htmlElement.getAttribute("data-bs-theme") === 'auto') {
-    function updateTheme() {
-      document.querySelector("html").setAttribute("data-bs-theme",
-        window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
-    }
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme)
-    updateTheme()
-  }
-})();
+// ; (function () {
+//   const htmlElement = document.querySelector("html")
+//   if (htmlElement.getAttribute("data-bs-theme") === 'auto') {
+//     function updateTheme() {
+//       document.querySelector("html").setAttribute("data-bs-theme",
+//         window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
+//     }
+//     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme)
+//     updateTheme()
+//   }
+// })();
 
 // toc
-$(document).ready(function () {
-  $('#toc').toc({ noBackToTopLinks: true });
-});
+
 
 
 // adiciona _blank e diversos
